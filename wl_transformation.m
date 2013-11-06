425b4896014a81bded44512b62b302aef46ad299
Roman Garnett <romanempire@gmail.com>
2013-11-06 16:02:05 +0100

    only finding as many primes as necessary

--- wl_transformation.m
+++ wl_transformation.m
@@ -1,6 +1,16 @@
 function new_labels = wl_transformation(A, labels)
 
-  log_primes = log(primes(1e4));
+  % the ith entry is equal to the 2^(i-1)'th prime
+  primes_arguments_required = [2, 3, 7, 19, 53, 131, 311, 719, 1619, ...
+                      3671, 8161, 17863, 38873, 84017, 180503, 386093, ...
+                      821641, 1742537, 3681131, 7754077, 16290047];
+
+  num_labels = max(labels);
+
+  % generate enough primes to have one for each label in the graph
+  primes_argument = primes_argument_required(ceil(log2(num_labels)) + 1);
+  p = primes(primes_argument);
+  log_primes = log(p(1:num_labels));
 
   [from, to] = find(A);
 
